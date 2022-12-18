2cca0918a397acdd58603938be8954a09a09395b
Divyesh Ladva <ladoodivyesh@gmail.com>
2022-12-18 16:44:33 +0530

    use environment port varible for elastic beanstalk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/test", (req, res, next) => {
   return res.send("works");
